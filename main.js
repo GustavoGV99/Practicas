56d08223d08e7ce6da67946cbcc7c4a8cdb595a3
GustavoGV <garciavanegasgustavoangel@gmail.com>
2024-10-18 20:36:17 -0600

    Se crean metodos y se añade JSDoc

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
 let a = 10;
-console.log(a);
+let b = "10";
+let c;
+let d;
+
+/**
+ * Método que se utiliza para sumar por parametros.
+ * @param {*} primerNumero es el primer valor de la sumar
+ * @param {*} b es el segundo valor para sumar
+ * @returns la suma de primerNumero + b
+ */
+function suma(primerNumero, b){
+  const num1 = a;
+  const num2 = b;
+
+  return num1 + num2;
+}
+
+/**
+ * Funcion que compara a y b
+ * si a es = a b entonces es true
+ * si no es false
+ */
+function comparar(){
+  c = a == b ? true : false;
+  // c = a == b ? "10" : a > 10 ? "no" : "si";
+  console.log(c);
+}
+suma();
+comparar();
+
+console.log(d ?? "d es vacio");
+//Combercion de tipos.
+console.log(8 + "1");
+console.log(8 - "1");
+console.log(8 * "Algo");
+console.log(true == 0);
+console.log(true == "Gus");
+
+
+
+
